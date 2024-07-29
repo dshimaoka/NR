8ce1529dbc59b2b29c85c12583ef360760b507b3
Daisuke Shimaoka <daisuke.shimaoka@monash.edu>
2024-07-29 16:36:16 +1000

    testing condSwitch=2 (daisuke)

--- nystagmusRivalry.m
+++ nystagmusRivalry.m
@@ -214,7 +214,7 @@ elseif strcmp(args.patchType,'grating')
     fm{1}.phaseSpeed = '@360*patch1.directionPolarity * patch1.speed/patch1.frameRate'; %[deg/frame]
     fm{2}.phaseSpeed = '@360*patch2.directionPolarity * patch2.speed/patch2.frameRate'; %[deg/frame] 
     % fm{2}.phase = '@patch1.phase + patch1.phaseSpeed*(patch1.frameRate+10*(1-patch2.physicalAlteration-patch2.congruent))*patch1.duration/1000 + 270';%*patch2.congruent'; %[deg]
-    fm{2}.phase = '@patch1.phase + patch1.phaseSpeed*(patch1.frameRate)*patch1.duration/1000 + 270' %works in BFS
+    fm{2}.phase = '@patch1.phase + patch1.phaseSpeed*(patch1.frameRate)*patch1.duration/1000 + 270' %works in condSwitch=0 not 1
 end
 
 %360*args.tf1List / c.screen.frameRate; % (deg/frame) TF = cycles/s, so spd = 360*TF / frameRate = (deg/s) / (fr/s)
