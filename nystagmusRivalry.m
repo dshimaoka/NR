603d3a56288b88514f6536032915562692930f77
Daisuke Shimaoka <daisuke.shimaoka@monash.edu>
2024-07-30 12:21:43 +1000

    alpha to be 0 (daisuke)

--- nystagmusRivalry.m
+++ nystagmusRivalry.m
@@ -92,7 +92,8 @@ fixationDeadline = 5000; %[ms] maximum time to initiate a trial
 iti = 1000; %[ms] inter trial interval
 
 %luminance correction
-redLuminance = 171/255; %Fraser ... Miller 2023
+% redLuminance = 171/255; %Fraser ... Miller 2023
+redLuminance = 0.33; %DS office
 
 %RDP
 dotSize = 1;%5; %dot size [pix]
@@ -201,8 +202,8 @@ fm{1}.addProperty('conditionSwitch', 1);
 
 fm{1}.addProperty('redFirst',0);
 %fm{1}.redFirst = plugins.jitter(c,{0, 1},'distribution','1ofN'); %NG always return 1
-fm{1}.color = '@[0.5*cic.redLuminance*patch1.redFirst 0.0 1-patch1.redFirst 0.5]'; 
-fm{2}.color = '@[1-patch1.redFirst 0.0 patch1.redFirst 0.5]'; 
+fm{1}.color = '@[cic.redLuminance*patch1.redFirst 0.0 1-patch1.redFirst 0]'; 
+fm{2}.color = '@[1-patch1.redFirst 0.0 patch1.redFirst 0]'; 
 fm{1}.on = '@fixstim.off'; %first stimulus
 fm{2}.on = '@patch1.on + cic.jitteredSOA'; %2nd stimulus
 fm{1}.duration = '@cic.tDur  - patch2.physicalAlteration * (cic.tDur - cic.jitteredSOA)';  %if physicalAlteration=1, terminate after jitteredSOA
