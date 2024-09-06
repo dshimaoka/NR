d10df1af1505537e0becc45e324584e16c68d51e
mauhagan <haganm@gmail.com>
2024-09-06 19:48:52 +1000

    neurostim now switched to superposition-binocular (joanita)

--- nystagmusRivalry.m
+++ nystagmusRivalry.m
@@ -64,7 +64,7 @@ nrDirectory = fileparts(mfilename('fullpath'));
 nsDirectory = strrep(nrDirectory,'NR','neurostim');
 originalHash = marmolab.getGitHash(nsDirectory);
 cd(nsDirectory);
-[~, cmdOutput] = system(sprintf('git show-ref superposition'));
+[~, cmdOutput] = system(sprintf('git show-ref superposition-binocular'));
 myHash = cmdOutput(1:40); %myHash = '141539c45b2263844e1e72ed9a4677b3cd19159f';
 system(sprintf('git checkout %s', myHash));
 cd(nrDirectory);
