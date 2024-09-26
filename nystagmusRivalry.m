778e4f6728a0b815c561db1b633c9f1789fd0d6b
mauhagan <haganm@gmail.com>
2024-09-26 19:11:37 +1000

    keyEcho=false (joanita)

--- nystagmusRivalry.m
+++ nystagmusRivalry.m
@@ -149,9 +149,8 @@ disp(['Expected duration ' num2str(nTotTime) '[s]']);
 c = marmolab.rigcfg('debug',args.debug, p.Unmatched); % set to false to save githash at start of each experiment!
 c.screen.overlayClut(4,:)=0; %do NOT show grids in subject screen
 c.screen.overlayClut(9,:)=1; %show eye position in console screen
-c.eye.useRawData = false;
-
-c.hardware.keyEcho = false;
+c.eye.useRawData = true; %true causes the bug
+c.hardware.keyEcho = false; %false
 c.saveEveryN = 1;
 
 c.paradigm = 'nystagmusRivalry';
