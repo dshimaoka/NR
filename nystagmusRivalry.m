4a5538a9cf24cb1f980220dea2c22cb34f04dc70
mauhagan <haganm@gmail.com>
2024-12-18 18:41:49 +1100

    c.screen.overlayClut(4,:)=0; c.screen.overlayClut(2,:)=0;c.screen.overlayClut(9,:)=1; (joanita)

--- nystagmusRivalry.m
+++ nystagmusRivalry.m
@@ -147,8 +147,9 @@ disp(['Expected duration ' num2str(nTotTime) '[s]']);
 
 %Create a Command and Intelligence Centre object (the central controller for everything). Here a cic is returned with some default settings for this computer, if it is recognized.
 c = marmolab.rigcfg('debug',args.debug, p.Unmatched); % set to false to save githash at start of each experiment!
-%c.screen.overlayClut(4,:)=0; %do NOT show grids in subject screen
-% c.screen.overlayClut(9,:)=1; %show eye position in console screen
+c.screen.overlayClut(4,:)=0; %do NOT show grids in subject screen
+c.screen.overlayClut(2,:)=0; %do NOT show eye position in console screen
+c.screen.overlayClut(9,:)=1; %show eye position in console screen
 c.eye.useRawData = true; %true causes the bug
 c.hardware.keyEcho = false; %false
 c.saveEveryN = 1;
